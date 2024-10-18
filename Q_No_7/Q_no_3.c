634ce722f95aebc193540d131b1ba737b20b9eba
Bibek Adhikari <113355321+adhikari10@users.noreply.github.com>
2024-10-18 10:38:59 +0300

    Add files via upload

--- Q_No_7/Q_no_3.c
+++ Q_No_7/Q_no_3.c
@@ -18,12 +18,12 @@ int main(){
     double input_x[size], output_y[size];
     int i;
     for(i = 0; i<size; i++){
-        input_x[i] = i *0.1;
+        input_x[i] = 0.1 * i;
         
     }
     
     for(i = 0; i<size; i++){
-        output_y[i] = sin(input_x[i] + 0.1* input_x[i]);
+        output_y[i] = sin(input_x[i]) + 0.1 * input_x[i];
         
     }
     make_csv("sin1_function.csv", output_y);
